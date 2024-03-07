4803a9b41a85e7877f9041d7ae2a7a0682385fde
FrancescoM95 <mon95fran@gmail.com>
2024-03-07 19:09:57 +0100

    added comics data

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,10 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
-});
+    return view('home');
+})->name('home');
+
+Route::get('movies', function () {
+    $movies = config('comics');
+    return view('movies', compact('movies'));
+})->name('movies');
