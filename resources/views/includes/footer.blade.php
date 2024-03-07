7276237d15c3f3d22f0a1611b3bf11dc758e2e8c
FrancescoM95 <mon95fran@gmail.com>
2024-03-07 19:09:21 +0100

    added includes

---
+++ resources/views/includes/footer.blade.php
@@ -0,0 +1,80 @@
+<footer>
+        <div id="footer-top">
+            <div class="container">
+                <div class="row">
+                    <div class="cols-2">
+                        <div class="row">
+                            <div class="cols-3">
+                                <h3>Dc comics</h3>
+                                <ul>
+                                    <li>Characters</li>
+                                    <li>Comics</li>
+                                    <li>Movies</li>
+                                    <li>TV</li>
+                                    <li>Games</li>
+                                    <li>Videos</li>
+                                    <li>News</li>
+                                </ul>
+
+                                <h3>Shop</h3>
+                                <ul>
+                                    <li>Shop dc</li>
+                                    <li>Shop dc collectibles</li>
+                                </ul>
+                            </div>
+                            <div class="cols-3">
+                                <h3>Dc</h3>
+                                <ul>
+                                    <li>Terms Of Use</li>
+                                    <li>Privacy policy (New)</li>
+                                    <li>Ad Choices</li>
+                                    <li>Advertising</li>
+                                    <li>Jobs</li>
+                                    <li>Subscriptions</li>
+                                    <li>Talent Workshops</li>
+                                    <li>CPSC Certificates</li>
+                                    <li>Ratings</li>
+                                    <li>Shop Help</li>
+                                    <li>Contact Us</li>
+                                </ul>
+                            </div>
+                            <div class="cols-3">
+                                <h3>Sites</h3>
+                                <ul>
+                                    <li>DC</li>
+                                    <li>MAD Magazine</li>
+                                    <li>DC Kids</li>
+                                    <li>DC Universe</li>
+                                    <li>DC Power Visa</li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="cols-2">
+                        <figure>
+                            <img src="{{Vite::asset('resources/img/dc-logo-bg.png')}}" alt="Logo DC bg" id="logo-footer">
+                        </figure>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div id="footer-bottom">
+            <div class="container">
+                <div class="row">
+                    <div class="cols-2">
+                        <button role="button">SIGN-UP NOW!</button>
+                    </div>
+                    <div class="cols-2 d-flex">
+                        <h2>FOLLOW US</h2>
+                        <ul id="social">
+                            <li><a href="#"><img src="{{Vite::asset('resources/img/footer-facebook.png')}}" alt="facebook"></a></li>
+                            <li><a href="#"><img src="{{Vite::asset('resources/img/footer-twitter.png')}}" alt="twitter"></a></li>
+                            <li><a href="#"><img src="{{Vite::asset('resources/img/footer-youtube.png')}}" alt="you tube"></a></li>
+                            <li><a href="#"><img src="{{Vite::asset('resources/img/footer-pinterest.png')}}" alt="pinterest"></a></li>
+                            <li><a href="#"><img src="{{Vite::asset('resources/img/footer-periscope.png')}}" alt="periscope"></a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </footer>
---
+++ resources/views/includes/header.blade.php
@@ -0,0 +1,21 @@
+<header class="container">
+    <figure>
+        <img src="{{Vite::asset('resources/img/dc-logo.png')}}" alt="Logo DC" id="logo">
+    </figure>
+    <nav>
+        <ul>
+            <li >
+                <a href="">CHARACTERS</a>
+                <a href="">COMICS</a>
+                <a href="{{route('movies')}}">MOVIES</a>
+                <a href="">TV</a>
+                <a href="">GAMES</a>
+                <a href="">COLLECTIBLES</a>
+                <a href="">VIDEOS</a>
+                <a href="">FANS</a>
+                <a href="">NEWS</a>
+                <a href="">SHOP</a>
+            </li>
+        </ul>
+    </nav>
+</header>
